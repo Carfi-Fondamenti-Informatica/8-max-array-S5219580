fef2e3bf9c6b709e9402e1f46c1a7570ddc3d65d
S5219580 <91791844+S5219580@users.noreply.github.com>
2021-12-07 13:06:37 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,14 +2,14 @@
 #include "lib.h"
 using namespace std;
 
-void inizia(int array[],int b){
+void inizia(float array[],int b){
     for(int i=0;i<b;i++){
         cin>> array[i];}
         return;
         }
 
-int maggiore(int array[],int b){
-int c=0;    
+float maggiore(float array[],int b){
+float c=0;    
 for(int i=0;i<b;i++){
 if(c<array[i])
 {c=array[i];}}

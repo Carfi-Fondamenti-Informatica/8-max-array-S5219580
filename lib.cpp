8e6cc85a8d60e8a166bfbd3cd276a189541ec150
S5219580 <91791844+S5219580@users.noreply.github.com>
2021-12-07 12:54:38 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+void inizia(int array[],int b){
+    for(int i=0;i<b;i++){
+        cin>> array[i];}
+        return;
+        }
+
+int maggiore(int array[],int b){
+int c=0;    
+for(int i=0;i<b;i++){
+if(c<array[i])
+{c=array[i];}}
+return c;
+}
